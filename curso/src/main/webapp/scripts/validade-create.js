532c593c48c801f3f6b6ccb82f512e5274d2fd79
Luis Henrique <luishcardoso228@gmail.com>
2024-04-17 21:29:12 -0300

    add-style

---
+++ curso/src/main/webapp/scripts/validade-create.js
@@ -0,0 +1,27 @@
+
+function changeFormStyle() {
+  const select = document.getElementById("type");
+  const selectedOption = select.options[select.selectedIndex].value;
+  const form = document.getElementById("user-form");
+  const userTypeLabel = document.getElementById("user-type-label");
+
+  // Remover classes de estilo anteriores
+  form.classList.remove("form-admin");
+  form.classList.remove("form-professor");
+  form.classList.remove("form-student");
+
+  // Adicionar classe de estilo com base na opção selecionada
+  if (selectedOption === "ADM") {
+      form.classList.add("form-admin");
+  } else if (selectedOption === "PROFESSOR") {
+      form.classList.add("form-professor");
+  } else if (selectedOption === "STUDENT") {
+      form.classList.add("form-student");
+  }
+
+  // Adicionar classe de animação à label, somente teste
+  userTypeLabel.classList.add("highlight-label");
+  setTimeout(function() {
+      userTypeLabel.classList.remove("highlight-label");
+  }, 500);
+}
