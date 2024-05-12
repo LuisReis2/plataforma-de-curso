9e0eb9155ae3daa278f318be7abe1c992140f707
Luis Henrique <luishcardoso228@gmail.com>
2024-05-12 04:03:01 -0300

    criando view da aula - parte 1

--- curso/src/main/java/br/com/curso/servlet/listAulaServlet.java
+++ curso/src/main/java/br/com/curso/servlet/listAulaServlet.java
@@ -3,6 +3,7 @@ package br.com.curso.servlet;
 import br.com.curso.dao.aulaDao;
 import br.com.curso.dao.userDao;
 import br.com.curso.model.Aula;
+import br.com.curso.model.Curso;
 import br.com.curso.model.User;
 
 import javax.servlet.ServletException;
@@ -18,12 +19,17 @@ import java.util.List;
 public class listAulaServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String idCurso = req.getParameter("idCurso");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String idCurso = req.getParameter("id");
 
-        List<Aula> aula = new aulaDao().listFkId(idCurso);
 
-        req.setAttribute("aula", aula);
+        List<Aula> aulas = new aulaDao().listFkId(idCurso);
+
+        for (Aula aula : aulas) {
+            System.out.println("Nome do Curso: " + aula.getIdAula());
+            System.out.println("oi");
+        }
+        req.setAttribute("aula", aulas);
         req.getRequestDispatcher("listAulas.jsp").forward(req, resp);
     }
 }
